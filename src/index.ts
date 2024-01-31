98f977cfccaea4a5db58443150a87f42b1f19003
djerada-madjid <djerada.madjid@gmail.com>
2024-01-31 14:26:37 +0100

    Back-Todo-T2: Create a hello world end point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hoho')
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log('test')
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
