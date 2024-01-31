bf7d5f1f0b667f054e33ddd98ee7a5f4086e4e5c
djerada-madjid <djerada.madjid@gmail.com>
2024-01-31 15:23:09 +0100

    Back-Todo-T3: Create a type and instance of Task, than display it with tasks endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express'
+import { tab } from './data'
 
 const app = express()
 const port = 3000
+
 
 app.get('/', (req, res) => {
     console.log('test')
   res.send('Hello World!')
 })
 
+app.get('/tasks',(req, res)=> {
+    res.send(tab)
+} )
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
