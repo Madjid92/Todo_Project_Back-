fee53f8e42758743b0227e4981c43d00126fef85
Madjid <djerada.madjid@gmail.com>
2024-02-09 18:14:44 +0100

    Back-Todo-T5:Eslint/Prettier installation and configuration

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { tab } from './data'
+import { Task } from './types'
 
 const app = express()
 const port = 3000
 
-
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response<string>) => {
   console.log('test')
   res.send('Hello World!')
 })
 
-app.get('/tasks',(req, res)=> {
-    res.send(tab)
-} )
+app.get('/tasks', (req: Request, res: Response<Task[]>) => {
+  res.send(tab)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
